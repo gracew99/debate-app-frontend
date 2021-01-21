435d29dcd671754a2c3b431693c54e3dc7cb1d7c
gracew99 <7.knicksfan.7@gmail.com>
2021-01-20 23:18:47 -0500

    small fixes to debate creation form

--- src/components/DebateCreation.jsx
+++ src/components/DebateCreation.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import axios from '../axios'
+import { useHistory } from "react-router-dom";
+import Form from "react-bootstrap/Form"
 
 function DebateCreation() {
 
@@ -13,6 +15,8 @@ function DebateCreation() {
     const [image2, setImage2]= useState("");
     const [descrip1, setDescrip1]= useState("");
     const [descrip2, setDescrip2]= useState("");
+    let history = useHistory();
+
 
     async function handleSubmit() {
         const params = {
@@ -28,8 +32,8 @@ function DebateCreation() {
             person1description: descrip1,  
             person2description: descrip2, 
         }
+        history.push('/');
         const response = await axios.post('/v2/posts', params);
-        console.log(response.data);
         setTopics(response.data);
         return response;
     }
@@ -37,49 +41,59 @@ function DebateCreation() {
     return (
         <div className="registerDebate">
             <h1 className="pageTitle"> Register a New Debate!</h1> <br/>
-            <form onSubmit={handleSubmit}>
-                <label>
-                Debater 1 Name: 
-                <input type="text" value={name1}  onChange={(e) => setName1(e.target.value)} />    </label>
-                <br/>
-                <label>
-                Debater 2 Name:
-                <input type="text" value={name2} onChange={(e) => setName2(e.target.value)} />     </label>
-                <br/>
-                <label>
-                Debate Title:
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />       </label>
-                <br/>
-                <label>
-                Debate Topics:
-                <input type="text" value={topics} onChange={(e) => setTopics(e.target.value)} />       </label>
-                <br/>
-                <label>
-                Link to Cover Image:
-                <input type="text" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />       </label>
-                <br/>
-                <label>
-                Date:
-                <input type="text" value={date} onChange={(e) => setDate(e.target.value)} />          </label>
-                <br/>
-                <label>
-                Link to Debater 1 Photo:
-                <input type="text" value={image1} onChange={(e) => setImage1(e.target.value)} />          </label>
-                <br/>
-                <label>
-                Link to Debater 2 Photo:
-                <input type="text" value={image2} onChange={(e) => setImage2(e.target.value)} />      </label>
-                <br/>
-                <label>
-                Debater 1 Description:
-                <textarea value={descrip1} onChange={(e) => setDescrip1(e.target.value)} />     </label>
-                <br/>
-                <label>
-                Debater 2 Description:
-                <textarea value={descrip2} onChange={(e) => setDescrip2(e.target.value)} />         </label>
-                <br/>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label> Debater 1 Name: 
+                        <input type="text" value={name1}  onChange={(e) => setName1(e.target.value)} />    
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Debater 2 Name:
+                        <input type="text" value={name2} onChange={(e) => setName2(e.target.value)} />   
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Debate Title:
+                        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} /> 
+                    </Form.Label>
+                </Form.Group>                
+                <Form.Group>
+                    <Form.Label> Debate Topics:
+                        <input type="text" value={topics} onChange={(e) => setTopics(e.target.value)} />  
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Link to Cover Image:
+                        <input type="text" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />   
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Date:
+                        <input type="text" value={date} onChange={(e) => setDate(e.target.value)} /> 
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Link to Debater 1 Photo:
+                        <input type="text" value={image1} onChange={(e) => setImage1(e.target.value)} /> 
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Link to Debater 2 Photo:
+                        <input type="text" value={image2} onChange={(e) => setImage2(e.target.value)} />  
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>  Debater 1 Description:
+                        <textarea value={descrip1} onChange={(e) => setDescrip1(e.target.value)} /> 
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Debater 2 Description:
+                        <textarea value={descrip2} onChange={(e) => setDescrip2(e.target.value)} />  
+                    </Form.Label>
+                </Form.Group>
                 <input type="submit" value="Submit" />
-            </form>
+            </Form>
         </div>
     )
 }
