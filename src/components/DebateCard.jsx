2c3a398fc3707a639c5eda90a51e374bc021e7b5
gracew99 <7.knicksfan.7@gmail.com>
2021-01-20 20:56:49 -0500

    convert to Dates, add happening today banner, styling

--- src/components/DebateCard.jsx
+++ src/components/DebateCard.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import { useHistory, useParams } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+
+const isToday = (someDateString) => {
+    const someDate = new Date(Date.parse(someDateString));
+    const today = new Date()
+    return someDate.getDate() === today.getDate() &&
+      someDate.getMonth() === today.getMonth() &&
+      someDate.getFullYear() === today.getFullYear()
+  }
+  
+
 
 function DebateCard(props) {
     let history = useHistory();
@@ -14,10 +25,13 @@ function DebateCard(props) {
 
     return (
         <div class="debateCard" style={{backgroundColor:props.color}}>
+            {isToday(props.date) &&   
+            <Badge className="badge" variant="success"> Happening Today! </Badge>}
+
             <h2>{props.person1 + " vs. " + props.person2}</h2>
             <h2>{props.title}</h2>
             <img src={props.imgurl} alt="debate"></img>
-            <p> {props.date}</p>
+            <p> {(new Date(Date.parse(props.date))).toLocaleDateString()}</p>
             <Button variant="info" className="viewDetails" onClick={getDetails}>View details</Button>
         </div>
     )
--- src/components/DebateDetails.jsx
+++ src/components/DebateDetails.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect} from 'react';
 import {useParams} from "react-router-dom"
 import axios from '../axios'
+import Badge from 'react-bootstrap/Badge'
+
+const isToday = (someDateString) => {
+    const someDate = new Date(Date.parse(someDateString));
+    const today = new Date()
+    return someDate.getDate() === today.getDate() &&
+      someDate.getMonth() === today.getMonth() &&
+      someDate.getFullYear() === today.getFullYear()
+}
 
 function DebateDetails(props) {
     // WARNING: must assign to exact param name from Route path 
@@ -20,21 +29,22 @@ function DebateDetails(props) {
     }, [])
     
     return (
-        <div>
+        <div className={'debateDetailContainer'}>
         {posts.map(post => 
             <div className={"debateDetail"} style={{backgroundColor:props.colors[0]}}>
-                <h2>{"Title: " + post.title}</h2>
-
-                <h2>{post.person1 + " vs. " + post.person2}</h2>
+                {isToday(post.date) &&   
+                <Badge className="badge" variant="success"> Happening Today! </Badge>}
+                <h1>{"Title: " + post.title}</h1>
+                <h1>{post.person1 + " vs. " + post.person2} <br/></h1>
+                <h4> {(new Date(Date.parse(post.date))).toLocaleDateString()} <br/><br/><br/></h4>
                 <img src={post.person1img} class="profilepic" alt="person1"></img>
-                <img src={post.person2img} class="profilepic" alt="person2"></img>
-                <h2>{post.topic}</h2>
-                <p>{post.date}</p>
-                <h2 className={"meet"}>{"Meet " + post.person1.split(" ")[0]}</h2>
-                <p className={"bio"}>{post.person1description}</p>
-                <h2 className={"meet"}>{"Meet " + post.person2.split(" ")[0]}</h2>
-                <p className={"bio"}>{post.person2description}</p>
-                <a href="signup"> Sign Up </a>
+                <img src={post.person2img} class="profilepic" alt="person2"></img><br/><br/><br/>
+                <h4>Topics: {post.topics.map(topic => topic)}</h4><br/>
+                <h1 className={"meet"}>{"Meet " + post.person1.split(" ")[0]}</h1>
+                <h4 className={"bio"}>{post.person1description}</h4> <br/>
+                <h1 className={"meet"}>{"Meet " + post.person2.split(" ")[0]}</h1>
+                <h4 className={"bio"}>{post.person2description}</h4>
+                <a href="signup" className="signUp"> Sign Up </a>
             </div>
         )}
         </div>
